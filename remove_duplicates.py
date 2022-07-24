55a7edf2eccae9f65bab7617b3921af0bbca7616
OsMax43 <109098127+OsMax43@users.noreply.github.com>
2022-07-24 23:56:19 +0300

    update

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -3,12 +3,20 @@ def remove_duplicates():
     array = [random.randint(0, 10) for i in range(int(input('Input list size:  ')))]
     print(array)
 
-    index = 1
-    while index < len(array):
-        if array[index] in array[ :index]:
-            array.pop(index)
-        else:
-            index += 1
-            print(array)
+    # index = 1
+    # while index < len(array):
+    #     if array[index] in array[ :index]:
+    #         array.pop(index)
+    #     else:
+    #         index += 1
+    #         print(array)
+
+    array2 = []
+    for i in array:
+        if i not in array2:
+            array2.append(i)
+            print(array2)
+
 
 remove_duplicates()
+
